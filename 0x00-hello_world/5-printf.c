a8affab9bee66f3e8ca9e764dcb97390d37cff16
Sokine <sokine_25@outlook.com>
2022-10-13 04:42:42 -0700

    Using the function printf to display

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#includ <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+         printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+}
