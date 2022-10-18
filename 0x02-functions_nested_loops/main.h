80ec4f49d09eda90d91f160324204cc02de97aa7
Sokine <sokine_25@outlook.com>
2022-10-18 15:37:58 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,9 +11,10 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
-int _putchar(char.c);
+void print_times_tabble(int n);
+void num(int r, int n);
 
 #endif
 
