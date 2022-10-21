6fdd7119d98c4da67f4d0d28d7a79660e1d2e987
Sokine <sokine_25@outlook.com>
2022-10-20 21:51:38 -0700

    a function that prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_number - takes an integer and prints it with _putchar
+ *@n: integer to print
+ * Return: Always 0
+ */
+void print_number(int n)
+
+{
+	int n;
+
+	_putchar("Enter number\n");
+	scanf("%d", &number);
+	_putchar("%d", n);
+
+}
