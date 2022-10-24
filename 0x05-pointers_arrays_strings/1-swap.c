b722013dbc41e1aa50fb831232b99d72e229b73b
Sokine <sokine_25@outlook.com>
2022-10-24 11:12:22 -0700

    a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+ * swap_int - swap values
+ * @a : pointer value
+ * @b : pointer value
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int prt;
+
+	prt = *a;
+	*a = *b;
+	*b = prt;
+
+}
+	
